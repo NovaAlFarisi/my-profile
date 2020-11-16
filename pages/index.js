499d7ef05527eeed3dac8b780f070788a8746443
Nova Al Farisi <novaa.alfarisi@gmail.com>
2020-11-16 22:26:04 +0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,39 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="container">
+      <div className="wrapper">
+        <div className="left-side">
+          <div className="nav">
+              <div className="nav-brand">
+                na<span>.</span>
+              </div>
+              <ul className="nav-items">
+                <li className="nav-item"><a href="#">Work</a></li>
+                <li className="nav-item"><a href="#">Story</a></li>
+                <li className="nav-item"><a href="#social">Contact</a></li>
+              </ul>
+            </div>
+            <div className="content">
+              <h1 className="my-name"><span>Nova Al</span> Farisi</h1>
+              <img src="/nopa.jpeg" alt="picture" className="image"/>
+              <p>I'm an junior full-stack programmer from mars. currently working at <b>PT. Klip Plastik Indonesia</b> as Web Developer.</p>
+            </div>
+            <div className="social" id="social">
+                <a href="https://api.whatsapp.com/send/?phone=628976007835&text=Hello%20Nova" target="_blank" className="button">Say Hello</a>
+                <div className="social-media">
+                  <a href="https://www.linkedin.com/in/nova-al-farisi-26a0b719b/" target="_blank" className="social-icon"><img src="/linkedin.png" alt="Social"/></a>
+                  <a href="https://instagram.com/nova_alfarisi" target="_blank" className="social-icon"><img src="/instagram.png" alt="Social"/></a>
+                  <a href="https://github.com/NovaAlFarisi" target="_blank" className="social-icon"><img src="/git.png" alt="Social"/></a>
+                </div>
+              </div>
+          </div>
+        <div className="right-side">
+          <div className="profile-img">
+            <img src="/nopa.jpeg" alt="picture" className="image"/>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
