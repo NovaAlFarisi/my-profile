973fb379959f73462b666d2984bdf64a8dbc94c5
Nova Al Farisi <novaa.alfarisi@gmail.com>
2020-11-24 20:27:28 +0700

    forget to change the page title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>Nova Al Farisi</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="container">
